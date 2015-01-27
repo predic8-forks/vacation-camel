15ca5bd321b1b58642d02de7da513d2723a0ca7d
Thomas Bayer <bayer@predic8.de>
2015-01-27 10:35:31 +0100

    minor refactorings

--- src/main/java/de/predic8/vacation/Routes.java
+++ src/main/java/de/predic8/vacation/Routes.java
@@ -1,50 +1,51 @@
 package de.predic8.vacation;
 
-import org.apache.camel.Exchange;
+import java.util.HashMap;
+
+import static org.apache.camel.Exchange.*;
 import org.apache.camel.builder.RouteBuilder;
+import org.jolokia.jvmagent.JolokiaServer;
+import org.jolokia.jvmagent.JolokiaServerConfig;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.w3c.dom.Document;
 
-/**
- * 
- *      Input JSON
- *       {
- *          "name": "Thomas",
- *          "city": "Bonn"
- *      }  
- *
- */
+import com.sun.xml.internal.xsom.impl.scd.Iterators.Map;
+
 public class Routes extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
 
-        from("jetty:http://localhost:8081/weathermap/")
+        from("jetty:http://localhost:8081/vacation/")
+        		.id("book") 
                 .unmarshal( "xmljson")
                 .setHeader( "city", xpath("//city/text()"))
                 .setHeader( "name", xpath("//name/text()"))
                 .log( "${header.name} wants to go to ${header.city}")
-                .setHeader( Exchange.HTTP_PATH, simple("data/2.5/find?q=${header.city}&units=metric&mode=xml"))
-                .setHeader( Exchange.HTTP_METHOD, constant("GET"))
+                .setHeader( HTTP_PATH, simple("data/2.5/find?q=${header.city}&units=metric&mode=xml"))
+                .setHeader( HTTP_METHOD, constant("GET"))
                 .to( "jetty:http://api.openweathermap.org/?bridgeEndpoint=true")
                 .convertBodyTo( Document.class)
                 .setHeader( "count", xpath("//count/text()", String.class))
-                .setHeader( "temp", xpath("//temperature/@value", String.class))
                 .to( "xslt:weather2booking.xsl")
                 .choice()
 	                .when( header( "count").isNotEqualTo( "1"))
-	                    .setHeader( Exchange.HTTP_RESPONSE_CODE, constant("400"))
+	                    .setHeader( HTTP_RESPONSE_CODE, constant("400"))
 	                    .setBody().simple(" { \"message\" : \"city not found\" }")
 	                    .endChoice()
-	                .when( simple( "${header.temp} > 20"))
+	                .when( xpath( "//temp > 20"))
 	                    .inOnly( "activemq:beach")
 	                    .setBody().simple( "{ \"message\" : \"Enjoy the sun!\" }")
 	                    .endChoice()
 	                .otherwise()
 	                    .inOnly( "activemq:ski")
 	                    .setBody().simple( "{ \"message\" : \"You go skiing!\" }")
-	                    .endChoice()
+	                    .endChoice()	
 	                .end();
+        
+        String qn = "startxy";
+        from("")
+        .to("jms:" + t);
 
 
     }
@@ -52,6 +53,12 @@ public class Routes extends RouteBuilder {
     @SuppressWarnings("all")
     public static void main(String[] args) throws Exception {
 
+    	HashMap map = new HashMap();
+    	map.put("port", "8181");
+    	JolokiaServerConfig config = new JolokiaServerConfig(map);
+    	JolokiaServer server = new JolokiaServer(config, true);
+    	server.start();
+    	
         new ClassPathXmlApplicationContext("camel.xml");
 
     }
